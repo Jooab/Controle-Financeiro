6ed9a7fa5f27c4bea79eece39e71a47413cbf5cd
joab-prates <joobascanal@gmail.com>
2025-05-17 18:15:20 -0300

    adicionando numero negativo no saldo

--- src/components/balance/index.tsx
+++ src/components/balance/index.tsx
@@ -6,13 +6,13 @@ const Balance = () => {
 
     const { entriesTotal, exitsTotal } = useContext(EntryExitContext);
 
-    console.log(entriesTotal, exitsTotal)
-
     const totalBalance = (entriesTotal - exitsTotal).toFixed(2);
+
+    const totalBalanceNumber = parseFloat(totalBalance);
 
     return (
         <Styled.Div>
-            <h1>Saldo : {parseFloat(totalBalance) > 0 && "R$" + totalBalance} </h1>
+            <h1>Saldo : R$ {totalBalanceNumber} </h1>
         </Styled.Div>
     );
 }
